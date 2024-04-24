70f26c1d4171c6d0d9f96a1d2e0db6bf50f0f458
pararan373 <pararamojajan@gmail.com>
2024-04-24 18:17:18 +0900

    いったんcommit

--- lib/page/start.dart
+++ lib/page/start.dart
@@ -1,3 +1,5 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_application_1/main.dart';
@@ -14,41 +16,28 @@ class  _StartState extends State<Start> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              Stack( 
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                      color: Colors.black,
-                      width: 3,
-                      )   
-                    ),
-                    width: 400,
-                    height: 400,
-                    child: Image.asset('assets/images/background_pig.png'),
+        child: Column(
+          children: [
+            Stack( 
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                    color: Colors.black,
+                    width: 3,
+                    )   
                   ),
-                  Column(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                          color: Colors.black,
-                          width: 3,
-                          ) 
-                        ),
-                        width: 350,
-                        height: 350,
-                        child: Image.asset('assets/images/logo.png'),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],//レイヤー
-          ),
+                  width: 400,
+                  height: 400,
+                  child: Image.asset('assets/images/background_pig.png'),
+                ),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: Image.asset('assets/images/logo.png')
+                ),
+              ],
+            ),
+          ],//レイヤー
         ),
       ),
     );
