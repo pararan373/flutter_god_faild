9653b1030e7cf854646ec06183aa4245822ce9d5
pararan373 <pararamojajan@gmail.com>
2024-04-24 13:37:47 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'page/start.dart';
+
+void main() {
+  //　一旦おまじないとしてスルーしてもらって大丈夫です
+  WidgetsFlutterBinding.ensureInitialized();
+  //これによってFlutterプロジェクトを起動する
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //デバックのバーを削除
+      debugShowCheckedModeBanner: false,
+      title: 'Noveler',
+      theme: ThemeData(
+        //テーマカラー : 青
+        primarySwatch: Colors.blue,
+      ),
+      // 初期画面に表示するページの指定
+      home: (Start()),
+    );
+  }
+}
---
+++ lib/page/start.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_application_1/main.dart';
+
+
+
+class Start extends StatefulWidget {
+  @override
+  _StartState createState() => _StartState();
+}
+
+class  _StartState extends State<Start> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Stack( 
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                      color: Colors.black,
+                      width: 3,
+                      )   
+                    ),
+                    width: 400,
+                    height: 400,
+                    child: Image.asset('assets/images/background_pig.png'),
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                          color: Colors.black,
+                          width: 3,
+                          ) 
+                        ),
+                        width: 350,
+                        height: 350,
+                        child: Image.asset('assets/images/logo.png'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],//レイヤー
+          ),
+        ),
+      ),
+    );
+  }
+}
