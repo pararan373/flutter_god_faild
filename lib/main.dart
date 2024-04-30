03ba04dad4d451b1e9c3b86714b2eba05e0706ab
Chuseshi-rookie <yu.nakamura.gamegod@gmail.com>
2024-04-30 21:48:26 +0900

    battleのスペルミスを変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/page/buttle.dart';
+import 'package:flutter_application_1/page/battle.dart';
 import 'page/start.dart';
 import 'package:go_router/go_router.dart';
+
 void main() {
-  
   //　一旦おまじないとしてスルーしてもらって大丈夫です
   WidgetsFlutterBinding.ensureInitialized();
   //これによってFlutterプロジェクトを起動する
   runApp(const MyApp());
 }
+
 final router = GoRouter(
   initialLocation: '/start',
-
   routes: [
     GoRoute(
       path: '/start',
-      builder: (context, state) => Start(),
+      builder: (context, state) => const Start(),
     ),
     GoRoute(
-      path: '/buttle',
-      builder: (context, state) => buttle(),
+      path: '/battle',
+      builder: (context, state) => const Battle(),
     ),
   ],
-);    
+);
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
