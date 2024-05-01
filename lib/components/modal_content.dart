c9c88dd4c192cc7f300296708165549a4482ed2e
Chuseshi-rookie <yu.nakamura.gamegod@gmail.com>
2024-05-01 09:17:53 +0900

    [bug]flutter run時にcontentが縮小するbugを修正

--- lib/components/modal_content.dart
+++ lib/components/modal_content.dart
@@ -6,40 +6,46 @@ class ModalContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 34, 93, 67),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () {
-            debugPrint("modalにてbackが押されました");
-            Navigator.of(context).pop(); // モーダルを閉じる
-          },
-        ),
-        actions: [
-          // ヘッダーボタンをここで追加
-          ModalButton(
-            onPressed: () {
-              // ボタンが押されたときの処理
-              debugPrint("startにて教典が押されました");
-            },
-          ),
-        ],
-      ),
-      body: Container(
+    return SafeArea(
+      child: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        decoration: const BoxDecoration(color: Colors.white),
-        child: const Center(
-          child: Text(
-            '全画面モーダル',
-            style: TextStyle(
-              color: Color.fromARGB(255, 34, 93, 67),
-              decoration: TextDecoration.none,
-              fontSize: 40.0,
-              fontWeight: FontWeight.w400,
+        color: Colors.white,
+        child: Column(
+          children: [
+            AppBar(
+              backgroundColor: const Color.fromARGB(255, 34, 93, 67),
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back, color: Colors.white),
+                onPressed: () {
+                  debugPrint("modalにてbackが押されました");
+                  Navigator.of(context).pop(); // モーダルを閉じる
+                },
+              ),
+              actions: [
+                // ヘッダーボタンをここで追加
+                ModalButton(
+                  onPressed: () {
+                    // ボタンが押されたときの処理
+                    debugPrint("startにて教典が押されました");
+                  },
+                ),
+              ],
             ),
-          ),
+            const Expanded(
+              child: Center(
+                child: Text(
+                  '全画面モーダル',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 34, 93, 67),
+                    decoration: TextDecoration.none,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
