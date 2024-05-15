e0aeee7983ab8f865ccf491cf1596f4c65584780
pararan373 <pararamojajan@gmail.com>
2024-05-15 15:38:20 +0900

    god_faild.dart内の手札配列を生成する関数genre_shuffleとcard_selectionを追加

--- lib/god_faild/action/draw_card.dart
+++ lib/god_faild/action/draw_card.dart
@@ -1,22 +1,20 @@
 import 'dart:math';
 
-void draw() {
-  final dice = {
-    "weapon":60,
-    "armor":30,
-    "goods":10,
-    "trade":30
-  };
+String draw() {
+  final dice = {"weapon": 60, "armor": 30, "goods": 10, "trade": 30};
+  String lottery;
 
   final sum = dice.values.reduce((value, element) => value + element);
   final rand = Random().nextInt(sum) + 1;
   var cumlativeSum = 0;
 
-  for (var entry in dice.entries){
+  for (var entry in dice.entries) {
     cumlativeSum += entry.value;
     if (cumlativeSum >= rand) {
       print('${entry.key}がでた。');
-      break;
+      lottery = entry.key;
+      return lottery;
     }
   }
+  throw Exception(e);
 }
---
+++ lib/god_faild/god_faild.dart
@@ -0,0 +1,56 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/god_faild/action/draw_card.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+String draw_genre = "";
+List hands = [];
+int numberOfWeapons = 50;
+int numberOfArmor = 30;
+int numberOfGoods = 10;
+int numberOfTrade = 3;
+
+List<String> genre_shuffle(){
+  List<String> genre = ["", "", "", "", "", ""];
+  for (int i = 0; i <6 ;i++){
+    genre[i]=draw();
+  }  
+  debugPrint('$genre');
+  return genre;
+}
+
+//riverPodで変数を監視するのさ
+  final db = FirebaseFirestore.instance;
+
+  List<String> card_selection(){
+    int numberOfCard = 0;
+    List cardType = genre_shuffle();
+    List<String> handCardIds = ["","","","","","",];
+
+    for(int i = 0;i<6;i ++){
+      // 乱数ジェネレーターのインスタンスを作成
+      var random = Random();
+      String genreInitial = "";
+      // 0からnumberOfCardまでの範囲で乱数を生成
+      switch(cardType[i]){
+        case 'weapon':
+          numberOfCard = numberOfWeapons;
+          genreInitial = "W";
+        case 'armor'  :
+          numberOfCard = numberOfArmor;
+          genreInitial = "A";
+        case 'trade'  :
+          numberOfCard = numberOfTrade;
+          genreInitial = "T";
+        case 'goods'  :
+          numberOfCard = numberOfGoods;
+          genreInitial = "G";
+      };
+
+      int randomNumber = random.nextInt(numberOfCard);
+      String document = genreInitial + randomNumber.toString();
+      handCardIds[i] = document;
+    }
+    debugPrint('$handCardIds');
+    return handCardIds;
+  }
